8e19686ce691ec7fe63d0e927ebf2637fcd19ec5
Mustafa Kutay Ebiclioglu <kutayeb@gmail.com>
2022-03-10 00:03:00 -0500

    Revert "Add calculator display logic"
    This reverts commit a516d48dca7ca0c7721fdaacba1dcf2f5a45d8a9.

--- javascript/main.js
+++ javascript/main.js
@@ -1,19 +1,10 @@
 import Calculator from "./objects/calculator.js";
 
-const calc = new Calculator();
 const numDisplay = document.querySelector(".display__numbers");
 const aggDisplay = document.querySelector(".display__agg");
-const numBtns = document.querySelectorAll(".btn--num");
 
 window.addEventListener("load", () => {
+    const calc = new Calculator();
     numDisplay.innerText = calc.display;
     aggDisplay.innerText = calc.aggregate;
 });
-
-[...numBtns]
-    .map(elem =>
-        elem.addEventListener("click", (event) => {
-            calc.setDisplay(event);
-            numDisplay.innerText = calc.getDisplay();
-        })
-    );
