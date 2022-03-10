b088f2aab6884ec237e2da78386bb64f8fbe2753
Mustafa Kutay Ebiclioglu <kutayeb@gmail.com>
2022-03-09 22:48:50 -0500

    Add event listener on load

--- javascript/main.js
+++ javascript/main.js
@@ -1,1 +1,10 @@
-console.log("hello world");
+import Calculator from "./objects/calculator.js";
+
+const numDisplay = document.querySelector(".display__numbers");
+const aggDisplay = document.querySelector(".display__agg");
+
+window.addEventListener("load", () => {
+    const calc = new Calculator();
+    numDisplay.innerText = calc.display;
+    aggDisplay.innerText = calc.aggregate;
+});
