a516d48dca7ca0c7721fdaacba1dcf2f5a45d8a9
Mustafa Kutay Ebiclioglu <kutayeb@gmail.com>
2022-03-09 23:28:43 -0500

    Add calculator display logic

--- javascript/objects/calculator.js
+++ javascript/objects/calculator.js
@@ -1,8 +1,20 @@
 function Calculator() {
-    this.display = 0;
-    this.aggregate = 0;
+    this.display = "0";
+    this.aggregate = "0";
     this.firstEntry = 0;
     this.secondEntry = 0;
+
+    this.setDisplay = (event) => {
+        if (this.display == 0) {
+            this.display = event.target.innerText;
+        } else if (this.display.length == 7){
+            return;
+        } else {
+            this.display += event.target.innerText;
+        }
+    };
+
+    this.getDisplay = () => (this.display);
 }
 
 export default Calculator;
