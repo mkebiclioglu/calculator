ad9eb610afbaa369d8f6dea400667541c02f4cb4
Mustafa Kutay Ebiclioglu <kutayeb@gmail.com>
2022-03-13 19:41:27 -0400

    Refactor addToDisplay function

--- javascript/objects/calculator.js
+++ javascript/objects/calculator.js
@@ -1,14 +1,14 @@
 function Calculator() {
-    this.display = 0;
-    this.aggregate = 0;
+    this.display = '0';
+    this.aggregate = '0';
     this.firstEntry = 0;
     this.secondEntry = 0;
 
     this.addToDisplay = function(e) {
         if (this.display == '0') {
-            this.display = e.target.innerText;
+            this.display = e.target.dataset.btn;
         } else if (this.display != '0') {
-            this.display += e.target.innerText;
+            this.display += e.target.dataset.btn;
         }
     };
 }
