eeec7f7713439ad6cea57a0d268b60db7e21c0c8
Mustafa Kutay Ebiclioglu <kutayeb@gmail.com>
2022-03-13 19:23:58 -0400

    Add event listeners to num btns

--- javascript/main.js
+++ javascript/main.js
@@ -2,9 +2,15 @@ import Calculator from "./objects/calculator.js";
 
 const numDisplay = document.querySelector(".display__numbers");
 const aggDisplay = document.querySelector(".display__agg");
+const numBtns = document.querySelectorAll(".btn--num");
 
-window.addEventListener("load", () => {
+window.addEventListener('load', () => {
     const calc = new Calculator();
     numDisplay.innerText = calc.display;
     aggDisplay.innerText = calc.aggregate;
 });
+
+[...numBtns]
+    .map(elem => elem.addEventListener('click', (e) => {
+        
+    }));
