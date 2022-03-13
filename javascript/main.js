815548098b736f6ed637eb8ba9e21b1ed12bd332
Mustafa Kutay Ebiclioglu <kutayeb@gmail.com>
2022-03-13 19:31:47 -0400

    Add calculator obj and display update

--- javascript/main.js
+++ javascript/main.js
@@ -3,14 +3,16 @@ import Calculator from "./objects/calculator.js";
 const numDisplay = document.querySelector(".display__numbers");
 const aggDisplay = document.querySelector(".display__agg");
 const numBtns = document.querySelectorAll(".btn--num");
+const calc = new Calculator();
 
 window.addEventListener('load', () => {
-    const calc = new Calculator();
     numDisplay.innerText = calc.display;
     aggDisplay.innerText = calc.aggregate;
 });
 
 [...numBtns]
     .map(elem => elem.addEventListener('click', (e) => {
-        
+        calc.updateDisplay(e);
+        numDisplay.innerText = calc.display;
+        aggDisplay.innerText = calc.aggregate;
     }));
--- javascript/objects/calculator.js
+++ javascript/objects/calculator.js
@@ -3,6 +3,14 @@ function Calculator() {
     this.aggregate = 0;
     this.firstEntry = 0;
     this.secondEntry = 0;
+
+    this.updateDisplay = function(e) {
+        if (this.display == '0') {
+            this.display = e.target.innerText;
+        } else if (this.display != '0') {
+            this.display += e.target.innerText;
+        }
+    };
 }
 
 export default Calculator;
