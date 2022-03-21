999baf63e3ac973e32e9376866b18ebba0532f8f
Mustafa Kutay Ebiclioglu <kutayeb@gmail.com>
2022-03-20 22:02:08 -0400

    Add listener on operator btns

--- javascript/main.js
+++ javascript/main.js
@@ -4,6 +4,7 @@ const numDisplay = document.querySelector(".display__numbers");
 const aggDisplay = document.querySelector(".display__agg");
 const numBtns = document.querySelectorAll(".btn--num");
 const sysBtns = document.querySelectorAll(".btn--system");
+const opBtns = document.querySelectorAll(".btn--operator");
 const calc = new Calculator();
 
 window.addEventListener('load', () => {
