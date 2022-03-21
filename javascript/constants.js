95bc8cbab99794f781351cb5ee97271431fd0cf3
Mustafa Kutay Ebiclioglu <kutayeb@gmail.com>
2022-03-20 22:02:34 -0400

    Delete constants

--- javascript/constants.js
+++
@@ -1,7 +0,0 @@
-export const ADD = 'ADD';
-export const SUBTRACT = 'SUBTRACT';
-export const DIVIDE = 'DIVIDE';
-export const MULTIPLY = 'MULTIPLY'
-export const RESET = 'RESET';
-export const CLEAR_ENTRY = 'CLEAR_ENTRY';
-export const DELETE = 'DELETE';
